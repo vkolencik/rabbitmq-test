ad800deb1c61cec4348e1b1f56708354cc09e620
Vojtěch Kolenčík <kolencik@gmail.com>
2018-12-02 23:44:27 +0100

    Add consumer

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,16 +5,31 @@ fun main(args: Array<String>) {
 }
 
 class Producer {
-    fun produce(msg: String) {
+
+    private val channel: Channel
+    private val exchangeName = "hello-exchange"
+    private val queueName = "hello-queue"
+
+    init {
         val factory = ConnectionFactory()
         val conn = factory.newConnection()
-        val channel = conn.createChannel()
-        val exchangeName = "hello-exchange"
+        channel = conn.createChannel()
         channel.exchangeDeclare(exchangeName, BuiltinExchangeType.DIRECT, true)
-        val queueName = "hello-queue"
         channel.queueDeclare(queueName, true, false, false, emptyMap())
         channel.queueBind(queueName, exchangeName, "hola")
 
-        channel.basicPublish("", "hola", null, msg.toByteArray())
+        channel.basicConsume(
+            queueName,
+            false,
+            DeliverCallback { consumerTag, message ->
+                println("Message: ${String(message.body)}")
+                channel.basicAck(message.envelope.deliveryTag, true)
+            },
+            CancelCallback {  }
+        )
+    }
+
+    fun produce(msg: String) {
+        channel.basicPublish(exchangeName, "hola", null, msg.toByteArray())
     }
 }
