bee184f2e57d175cd1e4e28b43731e77fe704e64
Vojtěch Kolenčík <vojtech.kolencik@e-invent.eu>
2018-12-09 13:25:35 +0100

    Add bindings parameter to Producer, add example with direct exchange

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,28 +1,55 @@
 fun main(args: Array<String>) {
 
-    directDefaultExchange()
+    defaultExchange()
+
+    directExchange()
+
+
 
 }
 
-private fun directDefaultExchange() {
+private fun defaultExchange() {
+    /*
+    Default exchange --> routing key directly states the queue name.
+    This binding is default, so it doesn't need to be declared.
+      */
+
+    println()
+    println("Default exchange")
+
     val queueName = "test-queue"
     Consumer(queueName).use {
         Consumer(queueName).use {
             Producer().use { producer ->
-                /*
-                 Direct exchange --> routing key directly states the queue name.
-                 This binding is default, so it doesn't need to be declared.
-                  */
 
                 producer.produce("Hello Prague", queueName)
                 producer.produce("Hello Berlin", queueName)
                 producer.produce("Hello Paris", queueName)
                 producer.produce("Hello London", queueName)
-                producer.produce("Hello Tokyo", queueName)
-                producer.produce("Hello Kairo", queueName)
-                producer.produce("Hello New York", queueName)
             }
         }
     }
 }
 
+fun directExchange() {
+    /*
+    Declare direct exchange and add binding that sends all messages with a given routing key to a specified queue.
+     */
+    val queueName = "test-queue"
+    val exchangeName = "test-exchange"
+    val routingKey = "hola"
+
+    println()
+    println("Direct exchange")
+
+    Consumer(queueName).use {
+        Consumer(queueName).use {
+            Producer(exchangeName, Producer.Binding(queueName, routingKey)).use { producer ->
+                producer.produce("Hello Prague", routingKey)
+                producer.produce("Hello Berlin", routingKey)
+                producer.produce("Hello Paris", routingKey)
+                producer.produce("Hello London", routingKey)
+            }
+        }
+    }
+}
--- src/main/kotlin/Producer.kt
+++ src/main/kotlin/Producer.kt
@@ -6,6 +6,7 @@ import java.io.Closeable
 @Suppress("CanBeParameter")
 class Producer(
     private val exchangeName: String = "",
+    vararg bindings: Binding,
     private val exchangeType: BuiltinExchangeType = BuiltinExchangeType.DIRECT
 ) : Closeable {
 
@@ -15,7 +16,10 @@ class Producer(
     init {
         if (exchangeName != "") // don't "declare" the default exchange
             channel.exchangeDeclare(exchangeName, exchangeType, true)
+
+        bindings.forEach { channel.queueBind(it.queueName, exchangeName, it.routingKey)}
     }
+
 
     fun produce(msg: String, routingKey: String) {
         channel.basicPublish(exchangeName, routingKey, null, msg.toByteArray())
@@ -25,4 +29,6 @@ class Producer(
         connection.close()
         // (channels are closed automatically with the connection)
     }
+
+    data class Binding(val queueName: String, val routingKey: String)
 }
